8fcdc8176cfeeb37ac881a66a2d81cb80947c690
kysaeed <kysaeed@gmail.com>
2024-11-30 16:35:49 +0900

    soft key

---
+++ resources/js/game/SoftKeyboard/Key.js
@@ -0,0 +1,9 @@
+export default class Keyboard {
+
+
+    constructor(scene) {
+
+
+    }
+
+}
---
+++ resources/js/game/SoftKeyboard/SoftKeyboard.js
@@ -0,0 +1,15 @@
+// import Phaser from 'phaser'
+
+import Key from './Key.js'
+
+export default class SoftKeyboard {
+
+
+    constructor(scene) {
+
+        this.base = scene.add.container(0,0)
+
+    }
+
+
+}
