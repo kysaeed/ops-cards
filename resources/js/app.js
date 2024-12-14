f7d31e2f55bf00b3f7132ad77312a3882cea0947
kysaeed <kysaeed@gmail.com>
2024-12-14 18:05:35 +0900

    画面回転

--- resources/js/app.js
+++ resources/js/app.js
@@ -112,6 +112,83 @@ console.log('to Next State : ', fetchData)
             })
         }
 
+
+        // 90度回転の中心点を設定
+        this.cameras.main.originX = 0.5
+        this.cameras.main.originY = (Const.Screen.Height * 0.5) / Const.Screen.Width
+
+        const isVertical = () => {
+
+            const game = window.game
+
+            const w = game.scale.parentSize.width
+            const h = game.scale.parentSize.height
+
+            let isVertical = false
+            if (w && h) {
+              if (w < h) {
+                isVertical = true
+              }
+            }
+
+            return isVertical
+        }
+
+        const setRotateState = (isRotate) => {
+            if (isRotate) {
+                // [|] 縦長スクリーンに表示
+                game.scale.displaySize.setAspectRatio( Const.Screen.Height/Const.Screen.Width );
+                game.scale.resize(Const.Screen.Height, Const.Screen.Width)
+                this.cameras.main.setRotation(Math.PI * 0.5)
+                game.scale.refresh()
+            } else {
+                // [--] 横長スクリーンに表示
+                game.scale.displaySize.setAspectRatio( Const.Screen.Width/Const.Screen.Height );
+                game.scale.resize(Const.Screen.Width, Const.Screen.Height)
+                this.cameras.main.setRotation(0)
+                game.scale.refresh()
+            }
+        }
+
+        const fit = () => {
+            /*
+            // PCの場合は、ディレプレイの持ち方を変えないので回転しない
+            if (game.device.os.desktop) {
+                return
+            }
+            */
+            setRotateState(isVertical())
+        }
+
+        let h
+        const onResize = () => {
+
+            if (h) {
+                clearTimeout(h)
+            }
+            h = setTimeout(() => {
+                fit()
+            }, 100)
+        }
+
+        window.onresize = () => {
+            onResize()
+        }
+
+        /*
+        // 旧仕様互換
+        window.onorientationchange = () => {
+        onResize()
+        }
+        */
+
+        screen.orientation.onchange = () => {
+            onResize()
+        }
+
+        // 回転の初期化
+        fit()
+
         axios.post('api/login', {}).then(() => {
             currentPhase.enter(this.duel, {}, toNextPhase)
         })
