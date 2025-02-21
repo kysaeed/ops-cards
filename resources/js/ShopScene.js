af91b8e87d4da42e63c94aa369e5c8768edf46a8
kysaeed <kysaeed@gmail.com>
2025-02-21 18:35:11 +0900

    Sceneの画面回転

--- resources/js/ShopScene.js
+++ resources/js/ShopScene.js
@@ -1,4 +1,6 @@
 import Const from './Const.js'
+import Card from './game/Card.js'
+
 
 const TitleScene = {
     key: 'ShopScene',
@@ -10,78 +12,69 @@ const TitleScene = {
 
     create() {
 
-        // 90度回転の中心点を設定
-        this.cameras.main.originX = 0.5
-        this.cameras.main.originY = (Const.Screen.Height * 0.5) / Const.Screen.Width
-
-
-        const isVertical = () => {
-
-            const game = window.game
-
-            const w = game.scale.parentSize.width
-            const h = game.scale.parentSize.height
-
-            let isVertical = false
-            if (w && h) {
-              if (w < h) {
-                isVertical = true
-              }
-            }
-
-            return isVertical
-        }
-
-        const setRotateState = (isRotate) => {
-            if (isRotate) {
-                // [|] 縦長スクリーンに表示
-                game.scale.displaySize.setAspectRatio( Const.Screen.Height/Const.Screen.Width );
-                game.scale.resize(Const.Screen.Height, Const.Screen.Width)
-                this.cameras.main.setRotation(Math.PI * 0.5)
-                game.scale.refresh()
-            } else {
-                // [--] 横長スクリーンに表示
-                game.scale.displaySize.setAspectRatio( Const.Screen.Width/Const.Screen.Height );
-                game.scale.resize(Const.Screen.Width, Const.Screen.Height)
-                this.cameras.main.setRotation(0)
-                game.scale.refresh()
-            }
-        }
-
-        const fit = () => {
-            /*
-            // PCの場合は、ディレプレイの持ち方を変えないので回転しない
-            if (game.device.os.desktop) {
-                return
-            }
-            */
-            setRotateState(isVertical())
-        }
-
-        let h
-        const onResize = () => {
-
-            if (h) {
-                clearTimeout(h)
-            }
-            h = setTimeout(() => {
-                fit()
-            }, 100)
-        }
-
-        window.onresize = () => {
-            onResize()
-        }
-
-        /*
-        // 旧仕様互換
-        window.onorientationchange = () => {
-        onResize()
-        }
-        */
-
-
         if (!game.device.os.desktop) { // PCの場合は、ディレプレイの持ち方を変えないので回転しない
+            // 90度回転の中心点を設定
+            this.cameras.main.originX = 0.5
+            this.cameras.main.originY = (Const.Screen.Height * 0.5) / Const.Screen.Width
+
+            const isVertical = () => {
+
+                const game = window.game
+
+                const w = game.scale.parentSize.width
+                const h = game.scale.parentSize.height
+
+                let isVertical = false
+                if (w && h) {
+                  if (w < h) {
+                    isVertical = true
+                  }
+                }
+
+                return isVertical
+            }
+
+            const setRotateState = (isRotate) => {
+                if (isRotate) {
+                    // [|] 縦長スクリーンに表示
+                    game.scale.displaySize.setAspectRatio( Const.Screen.Height/Const.Screen.Width );
+                    game.scale.resize(Const.Screen.Height, Const.Screen.Width)
+                    this.cameras.main.setRotation(Math.PI * 0.5)
+                    game.scale.refresh()
+                } else {
+                    // [--] 横長スクリーンに表示
+                    game.scale.displaySize.setAspectRatio( Const.Screen.Width/Const.Screen.Height );
+                    game.scale.resize(Const.Screen.Width, Const.Screen.Height)
+                    this.cameras.main.setRotation(0)
+                    game.scale.refresh()
+                }
+            }
+
+            const fit = () => {
+                /*
+                // PCの場合は、ディレプレイの持ち方を変えないので回転しない
+                if (game.device.os.desktop) {
+                    return
+                }
+                */
+                setRotateState(isVertical())
+            }
+
+            let h
+            const onResize = () => {
+
+                if (h) {
+                    clearTimeout(h)
+                }
+                h = setTimeout(() => {
+                    fit()
+                }, 100)
+            }
+
+            window.onresize = () => {
+                onResize()
+            }
+
             screen.orientation.onchange = () => {
                 onResize()
             }
--- resources/js/TitleScene.js
+++ resources/js/TitleScene.js
@@ -10,78 +10,70 @@ const TitleScene = {
 
     create() {
 
-        // 90度回転の中心点を設定
-        this.cameras.main.originX = 0.5
-        this.cameras.main.originY = (Const.Screen.Height * 0.5) / Const.Screen.Width
-
-
-        const isVertical = () => {
-
-            const game = window.game
-
-            const w = game.scale.parentSize.width
-            const h = game.scale.parentSize.height
-
-            let isVertical = false
-            if (w && h) {
-              if (w < h) {
-                isVertical = true
-              }
-            }
-
-            return isVertical
-        }
-
-        const setRotateState = (isRotate) => {
-            if (isRotate) {
-                // [|] 縦長スクリーンに表示
-                game.scale.displaySize.setAspectRatio( Const.Screen.Height/Const.Screen.Width );
-                game.scale.resize(Const.Screen.Height, Const.Screen.Width)
-                this.cameras.main.setRotation(Math.PI * 0.5)
-                game.scale.refresh()
-            } else {
-                // [--] 横長スクリーンに表示
-                game.scale.displaySize.setAspectRatio( Const.Screen.Width/Const.Screen.Height );
-                game.scale.resize(Const.Screen.Width, Const.Screen.Height)
-                this.cameras.main.setRotation(0)
-                game.scale.refresh()
-            }
-        }
-
-        const fit = () => {
-            /*
-            // PCの場合は、ディレプレイの持ち方を変えないので回転しない
-            if (game.device.os.desktop) {
-                return
-            }
-            */
-            setRotateState(isVertical())
-        }
-
-        let h
-        const onResize = () => {
-
-            if (h) {
-                clearTimeout(h)
-            }
-            h = setTimeout(() => {
-                fit()
-            }, 100)
-        }
-
-        window.onresize = () => {
-            onResize()
-        }
-
-        /*
-        // 旧仕様互換
-        window.onorientationchange = () => {
-        onResize()
-        }
-        */
-
-
         if (!game.device.os.desktop) { // PCの場合は、ディレプレイの持ち方を変えないので回転しない
+            // 90度回転の中心点を設定
+            this.cameras.main.originX = 0.5
+            this.cameras.main.originY = (Const.Screen.Height * 0.5) / Const.Screen.Width
+
+
+            const isVertical = () => {
+
+                const game = window.game
+
+                const w = game.scale.parentSize.width
+                const h = game.scale.parentSize.height
+
+                let isVertical = false
+                if (w && h) {
+                  if (w < h) {
+                    isVertical = true
+                  }
+                }
+
+                return isVertical
+            }
+
+            const setRotateState = (isRotate) => {
+                if (isRotate) {
+                    // [|] 縦長スクリーンに表示
+                    game.scale.displaySize.setAspectRatio( Const.Screen.Height/Const.Screen.Width );
+                    game.scale.resize(Const.Screen.Height, Const.Screen.Width)
+                    this.cameras.main.setRotation(Math.PI * 0.5)
+                    game.scale.refresh()
+                } else {
+                    // [--] 横長スクリーンに表示
+                    game.scale.displaySize.setAspectRatio( Const.Screen.Width/Const.Screen.Height );
+                    game.scale.resize(Const.Screen.Width, Const.Screen.Height)
+                    this.cameras.main.setRotation(0)
+                    game.scale.refresh()
+                }
+            }
+
+            const fit = () => {
+                /*
+                // PCの場合は、ディレプレイの持ち方を変えないので回転しない
+                if (game.device.os.desktop) {
+                    return
+                }
+                */
+                setRotateState(isVertical())
+            }
+
+            let h
+            const onResize = () => {
+
+                if (h) {
+                    clearTimeout(h)
+                }
+                h = setTimeout(() => {
+                    fit()
+                }, 100)
+            }
+
+            window.onresize = () => {
+                onResize()
+            }
+
             screen.orientation.onchange = () => {
                 onResize()
             }
@@ -97,7 +89,7 @@ const TitleScene = {
             'bg_title'
         )
 
-        const txtTitle = this.add.text(200, 100, 'カードゲームのタイトル画面', { fontSize: '42px', fill: '#000' });
+        const txtTitle = this.add.text(200, 150, 'カードゲームのタイトル画面', { fontSize: '42px', fill: '#000' });
         const txtSub = this.add.text(450, 400, '画面をクリック！', { fontSize: '20px', fill: '#000' });
 
         this.input.on('pointerdown', () => {
@@ -106,8 +98,6 @@ const TitleScene = {
             this.scene.start('ShopScene')
 
         })
-
-
 
 
     },
