1638273086c322b46546c27571177033bca95f7f
kysaeed <kysaeed@gmail.com>
2025-02-22 17:07:00 +0900

    shop 仮表示

--- resources/js/ShopScene.js
+++ resources/js/ShopScene.js
@@ -1,13 +1,54 @@
 import Const from './Const.js'
-import Card from './game/Card.js'
+import CardSprite from './game/CardSprite.js'
+import CardList from './game/CardList'
 
 
-const TitleScene = {
+const ShopScene = {
     key: 'ShopScene',
     active: false,
 
     preload() {
+
+        this.load.spritesheet('number', 'assets/number.png', {
+            frameWidth: 20,
+            frameHeight: 20,
+        });
+
         this.load.image('bg_shop', 'assets/bg_shop.png')
+
+        this.load.image('card', 'assets/card.png') // (160 * 220) * 0.5
+        this.load.image('card_pow', 'assets/card_pow.png')
+        this.load.image('card_type', 'assets/card_type.png')
+        this.load.image('card_back', 'assets/card_back.png')
+        this.load.image('card_shadow', 'assets/card_shadow.png')
+        this.load.image('deck_shadow', 'assets/deck_shadow.png');
+        this.load.image('card_tip', 'assets/card_tip.png')
+        this.load.image('card_tip_shadow', 'assets/card_tip_shadow.png')
+        this.load.image('deck_clickable', 'assets/deck_clickable.png')
+
+        /**
+         * todo :
+         *　  表示するカードだけプリロードする？
+         */
+         this.load.image('chara', 'assets/chara.png')
+         this.load.image('ch_kage', 'assets/ch_kage.png')
+         this.load.image('ch_magi', 'assets/ch_magi.png')
+         this.load.image('ch_whell', 'assets/ch_whell.png')
+         this.load.image('ch_eye', 'assets/ch_eye.png')
+         this.load.image('ch_oddc', 'assets/ch_oddc.png')
+         this.load.image('ch_snake', 'assets/ch_snake.png')
+         this.load.image('ch_moon', 'assets/ch_moon.png')
+         this.load.image('ch_mono', 'assets/ch_mono.png')
+         this.load.image('ch_star', 'assets/ch_star.png')
+         this.load.image('ch_mass', 'assets/ch_mass.png')
+         this.load.image('ch_db', 'assets/ch_db.png')
+         this.load.image('ch_clown', 'assets/ch_clown.png')
+         this.load.image('ch_dog', 'assets/ch_dog.png')
+         this.load.image('ch_machine', 'assets/ch_machine.png')
+         this.load.image('ch_scarecrow', 'assets/ch_scarecrow.png')
+         this.load.image('ch_frasco', 'assets/ch_frasco.png')
+         this.load.image('ch_parade', 'assets/ch_parade.png')
+         this.load.image('cat', 'assets/cat.png')
     },
 
     create() {
@@ -84,8 +125,8 @@ const TitleScene = {
         }
 
 
-        const txtTitle = this.add.text(200, 100, 'カードゲームのタイトル画面', { fontSize: '42px', fill: '#fff' });
-        const txtSub = this.add.text(450, 400, '画面をクリック！', { fontSize: '20px', fill: '#fff' });
+        // const txtTitle = this.add.text(200, 100, 'カードゲームのタイトル画面', { fontSize: '42px', fill: '#fff' });
+        // const txtSub = this.add.text(450, 400, '画面をクリック！', { fontSize: '20px', fill: '#fff' });
 
         this.input.on('pointerdown', () => {
             //
@@ -100,10 +141,42 @@ const TitleScene = {
             'bg_shop'
         )
 
+        for (let i=0; i < 3; i++) {
+            const c = new CardSprite(this, CardList[i])
+
+            c.sprite.x = 200 + (i * 120)
+            c.sprite.y = 200
+            c.sprite.scale = 0.55
+
+            c.onUpdate()
+        }
+
+
+        let x = 0
+        let y = 0
+        for (let i=1; i <= 20; i++) {
+            const c = new CardSprite(this, CardList[i % 10])
+
+            c.sprite.x = x + 180
+            c.sprite.y = y + 380
+            c.sprite.scale = 0.34
+            const Bevel = 20
+            c.sprite.angle = (Math.random() * Bevel) - (Bevel * 0.5)
+
+            c.onUpdate()
+
+            x += 60
+            if (!(i % 10)) {
+                x = 0
+                y += 100
+            }
+        }
     },
 
-    update() {},
+    update() {
+        //
+    },
 
 }
 
-export default TitleScene
+export default ShopScene
